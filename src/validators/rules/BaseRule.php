e5a76698563c50921ada4202c8a01661edf0c8d4
viktorprogger <viktorprogger@gmail.com>
2018-12-19 14:52:01 +0300

    Preparing to validate by Rule class

--- src/validators/rules/BaseRule.php
+++ src/validators/rules/BaseRule.php
@@ -3,9 +3,42 @@
 namespace yii\validators\rules;
 
 use yii\base\BaseObject;
+use yii\exceptions\InvalidConfigException;
 
 class BaseRule extends BaseObject
 {
     public $value;
     public $message;
+    protected $validationMethod;
+    protected $regex;
+
+    /**
+     * @param $value
+     *
+     * @return bool
+     * @throws InvalidConfigException
+     */
+    public function validate($value): bool
+    {
+        if (!empty($this->validationMethod)) {
+            return call_user_func_array($this->validationMethod, [$this->value, $value]);
+        }
+
+        if (empty($this->regex)) {
+            $message = 'You must have either validationMethod or regex set for ' . static::class;
+            throw new InvalidConfigException($message);
+        }
+
+        return $this->validateRegex($value);
+    }
+
+    public function setValidationMethod(Callable $method)
+    {
+        $this->validationMethod = $method;
+    }
+
+    protected function validateRegex($value): bool
+    {
+        return preg_match($this->regex, $value);
+    }
 }
